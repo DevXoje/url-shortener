2e9edb6d98e3bccbff1d41e4e7e4a868d370333b
Xoje-Laptop <dawxoje@gmail.com>
2024-01-20 19:41:52 +0100

    Add short-unique-id package dependency

--- src/model/classes/UrlShortener.ts
+++ src/model/classes/UrlShortener.ts
@@ -1,7 +1,7 @@
 import type { URLShortenerModel } from '../interfaces/URLShortenerModel';
 import { URLShortenerDTO } from './URLShortenerDTO';
-/* import { ShortUniqueId } from 'short-unique-id'; */
-//const { randomUUID } = new ShortUniqueId({ length: 10 });
+import ShortUniqueId from 'short-unique-id';
+const { randomUUID } = new ShortUniqueId({ length: 10 });
 export class URLShortener implements URLShortenerModel {
     urlBase: URL;
     urlOriginal: URL;
@@ -9,16 +9,14 @@ export class URLShortener implements URLShortenerModel {
     constructor(urlOriginal: URL | string) {
         this.urlBase = new URL('https://localhost:3000');
         this.urlOriginal = urlOriginal instanceof URL ? urlOriginal : new URL(urlOriginal);
-        console.log(this.urlOriginal);
-        this.urlShortened = this.generateUrlShortened(this.urlOriginal);
+        this.urlShortened = this.generateUrlShortened(this.urlBase);
         this.saveUrlShortened(this);
     }
 
-    private generateUrlShortened(urlOriginal: URL): URL {
-        const stringUrlOriginal = urlOriginal.toString();
-        const text = `${stringUrlOriginal}${stringUrlOriginal}${stringUrlOriginal}${stringUrlOriginal}`;
-        const urlShortened = new URL(text);
-        return urlShortened;
+    private generateUrlShortened(urlBase: URL): URL {
+        const randomValue = randomUUID();
+        urlBase.pathname = randomValue;
+        return urlBase;
     }
     private saveUrlShortened(urlToSave: URLShortener): void {
         const urlDTO = new URLShortenerDTO(urlToSave);
