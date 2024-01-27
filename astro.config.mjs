065c8f8b9db7ad4c16a0b65820ae666a498a5d2c
Xoje-Laptop <dawxoje@gmail.com>
2024-01-27 15:48:15 +0100

    Refactor Astro config and add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-;
-import vue from "@astrojs/vue";
-import node from "@astrojs/node";
-
 import vercel from "@astrojs/vercel/serverless";
+import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind({
-    configFile: './tailwind.config.mjs',
-    applyBaseStyles: true,
-    nesting: true
-  }), vue()],
+  integrations: [
+    tailwind({
+      configFile: "./tailwind.config.mjs",
+      applyBaseStyles: true,
+      nesting: true,
+    }),
+    vue(),
+  ],
   redirects: {
-    "/[...pathName]": "/api/[...pathName]"
-  }
+    "/": "/pages/index",
+    "/[...pathName]": "/api/[...pathName]",
+  },
 });
