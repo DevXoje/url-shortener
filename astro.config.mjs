bd2c7eb70f1e9c62c91b9b860cab5e2725683201
Xoje-Laptop <dawxoje@gmail.com>
2024-01-27 15:39:05 +0100

    Add @astrojs/vercel package and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 ;
 import vue from "@astrojs/vue";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
-  integrations: [
-    tailwind({
-      configFile: './tailwind.config.mjs',
-      applyBaseStyles: true,
-      nesting: true
-    }),
-    vue()
-  ],
+  adapter: vercel(),
+  integrations: [tailwind({
+    configFile: './tailwind.config.mjs',
+    applyBaseStyles: true,
+    nesting: true
+  }), vue()],
   redirects: {
     "/[...pathName]": "/api/[...pathName]"
   }
